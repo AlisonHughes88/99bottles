f68f247799c538654b6640415f6ba092e09958c3
AlisonHughes88 <100470115+AlisonHughes88@users.noreply.github.com>
2022-04-06 19:08:33 +0100

    Add files via upload

---
+++ beerbottles.js
@@ -0,0 +1,31 @@
+/*function count() {
+	for(i=0;i<=10;i=i+1){
+		document.getElementById("").innerHTML+="<br />"+i+",";
+}
+	
+}
+/*
+public class beer {
+    public static void main(String[] args) {
+        //var
+        int beerNum = 99;
+
+        while (beerNum > 0) { //there are bottles left
+
+            if (beerNum == 1) { beerNum = 1;} // 1 bottle left, singular bottle
+
+            System.out.println(beerNum + " bottles of beer on the wall, " + beerNum + " bottles of beer.");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 0) {
+                System.out.println("Take one down, pass it round " + beerNum + " bottles of beer.");
+            }
+              }
+
+       			 if (beerNum == 0) {
+           		 System.out.println("No more bottles of beer.");// 0 bottles left
+       		 }
+
+   		 }
+	}
+	*/
